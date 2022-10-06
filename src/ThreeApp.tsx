1a55ab243c2fc462c72227218865e0cea81eb70e
Nathan Babcock <nathan.r.babcock@gmail.com>
2022-10-06 10:31:20 -0500

    feat: add react-three-fiber and three

---
+++ src/ThreeApp.tsx
@@ -0,0 +1,35 @@
+import type { ThreeElements } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import type { Mesh } from 'three'
+
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame(() => (ref.current.rotation.x += 0.01))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function ThreeApp() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.scss'
+import ThreeApp from './ThreeApp'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThreeApp />
   </React.StrictMode>
 )
